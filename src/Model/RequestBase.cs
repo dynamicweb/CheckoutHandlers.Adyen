c8b82bd6becb2131861b6b8eae587daefb57035b
Dmitriy Benyuk <dbe@dynamicweb-cms.com>
2024-03-27 17:28:23 +0200

    Remove Json Settings

--- src/Model/RequestBase.cs
+++ src/Model/RequestBase.cs
@@ -7,20 +7,7 @@ namespace Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Model
 {
     [DataContract]
     public abstract class RequestBase
-    {
-        /// <summary>
-        /// Compact serializer settings. Do not excludes default value.
-        /// </summary>
-        [Obsolete("Don't use it.")]
-        protected static readonly JsonSettings JsonSettings = new JsonSettings
-        {
-            Formatting = Formatting.None,
-            NullValueHandling = NullValueHandling.Ignore,
-            DefaultValueHandling = DefaultValueHandling.Include,
-            MissingMemberHandling = MissingMemberHandling.Ignore,
-            StringEscapeHandling = StringEscapeHandling.EscapeNonAscii,
-        };
-
+    {        
         [DataMember(Name = "merchantAccount")]
         public string MerchantAccount { get; set; }
 
