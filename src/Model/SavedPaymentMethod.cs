06bdc665246584191a512ecd10a87451c1bb4ad9
Stanislav Smetanin <ssm@dynamicweb-cms.com>
2024-05-15 15:47:03 +1000

    Adyen made mandatory to use security code for one-click payments. Added template for this, and code to handle this situation. Fixed update provider.

--- src/Model/SavedPaymentMethod.cs
+++ src/Model/SavedPaymentMethod.cs
@@ -1,23 +1,49 @@
 ﻿using System.Runtime.Serialization;
 
-namespace Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Model
+namespace Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Model;
+
+[DataContract]
+public class SavedPaymentMethod
 {
-    [DataContract]
-    public class SavedPaymentMethod
+    [DataMember(Name = "id", EmitDefaultValue = false)]
+    public string CardToken { get; set; }
+
+    [DataMember(Name = "type", EmitDefaultValue = false)]
+    public string PaymentMethodType { get; set; }
+
+    [DataMember(Name = "brand", EmitDefaultValue = false)]
+    public string Brand { get; set; }
+
+    [DataMember(Name = "expiryMonth", EmitDefaultValue = false)]
+    public string ExpiryMonth { get; set; }
+
+    [DataMember(Name = "expiryYear", EmitDefaultValue = false)]
+    public string ExpiryYear { get; set; }
+
+    [DataMember(Name = "holderName", EmitDefaultValue = false)]
+    public string HolderName { get; set; }
+
+    [DataMember(Name = "lastFour", EmitDefaultValue = false)]
+    public string LastFour { get; set; }
+
+    [DataMember(Name = "name", EmitDefaultValue = false)]
+    public string Name { get; set; }
+
+    [DataMember(Name = "networkTxReference", EmitDefaultValue = false)]
+    public string NetworkTxReference { get; set; }
+
+    [DataMember(Name = "supportedShopperInteractions", EmitDefaultValue = false)]
+    public string[] SupportedShopperInteractions { get; set; }
+
+    [DataMember(Name = "supportedRecurringProcessingModels", EmitDefaultValue = false)]
+    public string[] SupportedRecurringProcessingModels { get; set; }
+
+    public PaymentMethod ToPaymentMethod()
     {
-        [DataMember(Name = "id", EmitDefaultValue = false)]
-        public string CardToken { get; set; }
-
-        [DataMember(Name = "type", EmitDefaultValue = false)]
-        public string PaymentMethodType { get; set; }
-
-        public PaymentMethod ToPaymentMethod()
+        return new()
         {
-            return new PaymentMethod
-            {
-                Type = PaymentMethodType,
-                StoredPaymentMethodId = CardToken,
-            };
-        }
+            Type = PaymentMethodType,
+            StoredPaymentMethodId = CardToken,
+        };
     }
 }
--- src/Updates/AdyenUpdateProvider.cs
+++ src/Updates/AdyenUpdateProvider.cs
@@ -1,19 +1,30 @@
 ﻿using Dynamicweb.Updates;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
-namespace Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Updates
+namespace Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Updates;
+
+public class AdyenUpdateProvider : UpdateProvider
 {
-    public class AdyenUpdateProvider : UpdateProvider
+    private static Stream GetResourceStream(string name)
     {
-        public override IEnumerable<Update> GetUpdates()
-        {
-            var type = GetType();
+        string resourceName = $"Dynamicweb.Ecommerce.CheckoutHandlers.Adyen.Updates.{name}";
 
-            return new List<Update>() {
-                new FileUpdate("1", this, "/Files/Templates/eCom7/CheckoutHandler/Adyen/Form/Payments.cshtml", () => {
-                    return type.Assembly.GetManifestResourceStream($"{type.Namespace}.Payments.cshtml");
-                })
-            };
-        }
+        return Assembly.GetAssembly(typeof(AdyenUpdateProvider)).GetManifestResourceStream(resourceName);
     }
+
+    public override IEnumerable<Update> GetUpdates()
+    {
+        return new List<Update>() {
+            new FileUpdate("3cd0b762-78fd-42cd-ad96-9d8729690bf8", this, "/Files/Templates/eCom7/CheckoutHandler/Adyen/Form/Payments.cshtml", () => GetResourceStream("Payments.cshtml")),
+            new FileUpdate("8b3cdbb9-d5d4-46a9-b323-33b266149448", this, "/Files/Templates/eCom7/CheckoutHandler/Adyen/Card/Card.cshtml", () => GetResourceStream("Card.cshtml"))
+        };
+    }
+
+    /*
+     * IMPORTANT!
+     * Use a generated GUID string as id for an update
+     * - Execute command in C# interactive window: Guid.NewGuid().ToString()
+     */
 }
